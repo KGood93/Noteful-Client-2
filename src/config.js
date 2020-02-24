9164f112b08e119498d054d0c62596834e2b81ff
KGood93 <goodreau.kath@gmail.com>
2020-02-24 09:44:23 -0600

    Add heroku endpoint

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-    API_ENDPOINT: `http://localhost:8000/api`,
+    API_ENDPOINT: `https://fast-shelf-79888.herokuapp.com/api`,
+    //API_ENDPOINT: `http://localhost:8000/api`,
   }
